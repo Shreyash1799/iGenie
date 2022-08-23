cebfa24fccd25d56472405df6e1522c548c11bda
Shreyash1799 <shreyash17shubham@gmail.com>
2022-08-24 02:32:28 +0530

    Interview_page1

---
+++ pages/checking/module.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+<Link
+          href={{
+            pathname: "pages\checking\index.tsx",
+            query: data, // the data
+          }}
+>
+         <a>jscashshhhshshhshshss</a> 
+</Link>
